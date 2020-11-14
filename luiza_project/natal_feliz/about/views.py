99542d39c4c8a14c4bf173b6fd255ebf5e900cc1
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-14 14:39:58 -0300

    home and about

--- luiza_project/natal_feliz/about/views.py
+++ luiza_project/natal_feliz/about/views.py
@@ -2,8 +2,28 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http.response import HttpResponse
 
+membros = [
+    {
+        'nome':'Isadora',
+        'job':'Masters student',
+        'job_comp':'USP',
+        'job-loc':'SP-Brazil'
+    },
+    {
+        'nome':'Regina',
+        'job':'Software Engineer',
+        'job_comp':'Gama',
+        'job_loc':'SP-Brazil'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Bem-vindo ao incrível site de coisinhas de Natal :) </h1>")
+    contexto = {
+        'membros':membros
+    } 
+    return render(request, 'home.html',contexto)
+
+
 def about(request):
     return HttpResponse("<h1>Site sobre coisinhas de Natal</h1>")
