3e43ba9b4d61fc3acbb2f246c638e1723e387f31
DESKTOP-2N7HPJ1\isado <isadora-gf@hotmail.com>
2020-11-16 19:41:43 -0300

    admin

--- luiza_test/luiza_website/shopping_list/models.py
+++ luiza_test/luiza_website/shopping_list/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):
-    titulo = models.CharField(max_length=100)
-    conteudo = models.TextField()
-    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+class Shopping_List(models.Model):
+    bought = models.BooleanField(default=False) #pra fazer o inativo
+    item = models.CharField(max_length=100)
 
+def __str__(self):
+    return self.titulo
